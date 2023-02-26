ed77a761657e2ceb1d51c9921bd264abcac47b58
Oran Levi <100801409+OranLevi@users.noreply.github.com>
2023-02-26 14:05:26 +0200

    Version 1.0

--- client/src/components/Converter.js
+++ client/src/components/Converter.js
@@ -1,63 +1,45 @@
-import { BsArrowLeftRight } from 'react-icons/bs';
+import { motion, AnimatePresence } from 'framer-motion';
+import useConverterRow from '../hooks/useConverterRow';
 
 const Converter = () => {
+
+    const { ConverterRow,
+        isErrorAlert,
+        isClickConvert,
+        errorMessage,
+        currencyFromDisplay,
+        currencyToDisplay,
+        currencyAmountToDisplay,
+        amountTotal,
+        lastUpdate } = useConverterRow()
+
+    const RatesRow = () => {
+        return (
+            <div className="text-center">
+                <h3>1.00 {currencyFromDisplay} = {currencyAmountToDisplay} {currencyToDisplay}</h3>
+                <h1>Conversion result: {amountTotal} {currencyToDisplay}</h1>
+                <p className='text-center mt-2'>Last Updated: {lastUpdate}</p>
+            </div>
+        );
+    }
+
+    const Error = (props) => {
+        return (
+            <div className="alert alert-danger">
+                <strong>Error!</strong> {props.errorMessage}
+            </div>
+        );
+    }
+
     return (
         <div className="bg-light shadow rounded border border-3 col-lg-6 col-md-8 col-sm-8 col-10 mt-5 m-auto converter-center">
-            <Rates />
+            <AnimatePresence>
+                {isErrorAlert && (<motion.div initial={{ opacity: 0, y: '100%' }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: '100%' }}  > <Error errorMessage={errorMessage} /> </motion.div>)}
+            </AnimatePresence>
+            <AnimatePresence>
+                {isClickConvert && (<motion.div initial={{ opacity: 0, y: '-100%' }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: '-100%' }} > <RatesRow /> </motion.div>)}
+            </AnimatePresence>
             <ConverterRow />
-        </div>
-    );
-}
-
-const Rates = () => {
-    return (
-        <div className="text-center">
-            <h2>1.00 USD = 4.4425 MYR</h2>
-            <h3>Date: Wed, 22 Feb 2023</h3>
-        </div>
-    );
-}
-
-const ConverterRow = () => {
-    return (
-        <form>
-        <div className="mt-3 px-4">
-            <div className="container">
-                <h5>Amount:</h5>
-                <input type="number" className="form-control" placeholder="Enter Amount" required />
-
-                <div className="row pt-3 d-flex justify-content-center text-center">
-                    <DropDownMenu curreny="USD" name="From:" />
-                    <div className="col-4 d-flex flex-column align-items-center justify-content-center">
-                        <BsArrowLeftRight size={40} />
-                    </div>
-                    <DropDownMenu curreny="MRY" name="To:" />
-                </div>
-
-                <div className="d-grid pt-5">
-                    <button className="btn btn-primary" type="submit">Convert</button>
-                </div>
-            </div>
-        </div>
-        </form>
-    );
-}
-
-const DropDownMenu = (props) => {
-    return (
-        <div className="col-4">
-            <h5>{props.name}</h5>
-            <div className="dropdown ">
-                <a className="btn btn-light btn-outline-secondary dropdown-toggle col-md-12" href="/#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                    {props.curreny}
-                </a>
-
-                <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    <li><a className="dropdown-item" href="/#">USD</a></li>
-                    <li><a className="dropdown-item" href="/#">MRY</a></li>
-                    <li><a className="dropdown-item" href="/#">EUR</a></li>
-                </ul>
-            </div>
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,39 @@
 const express = require("express");
+const https = require('https');
+const cors = require('cors');
+
+require('dotenv').config();
 
 const app = express();
+app.use(cors());
 
-app.listen(3001,() =>{
+app.get("/getData/:from/:to", (req, res) => {
+    const selectedCurrencyFrom = req.params.from;
+    const selectedCurrencyTo = req.params.to;
+
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${selectedCurrencyFrom}`;
+
+    https.get(url, (httpsRes) => {
+      let body = "";
+      httpsRes.on("data", (chunk) => {
+        body += chunk;
+      });
+      httpsRes.on("end", () => {
+        try {
+          let data = JSON.parse(body);
+          console.log(data.conversion_rates[selectedCurrencyTo]);
+          res.send(data);
+        } catch (error) {
+          console.error(error.message);
+          res.status(500).send('Internal Server Error');
+        };
+      });
+    }).on("error", (error) => {
+      console.error(error.message);
+      res.status(500).send('Internal Server Error');
+    });
+  });
+
+app.listen(3001, () => {
     console.log("SERVER RUNS");
 });
